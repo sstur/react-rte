501d24bfe1067ca805092bb118ed1be35455a238
Simon Sturmer <sstur@me.com>
2016-02-25 17:20:12 +0700

    refactor EditorExample into RichTextEditor and add flow types

--- src/EditorExample.js
+++ src/EditorExample.js
@@ -1,181 +1,32 @@
+/* @flow */
 import React from 'react';
-import Draft from 'draft-js';
+import RichTextEditor from './lib/RichTextEditor';
+
+import type {EditorState} from 'draft-js';
 
 const {Component} = React;
 
-export default class EditorExample extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-
-    this.focus = () => this.refs.editor.focus();
-    this.onChange = (editorState) => this.setState({editorState});
-
-    this.handleKeyCommand = (command) => this._handleKeyCommand(command);
-    this.toggleBlockType = (type) => this._toggleBlockType(type);
-    this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
-  }
-
-  _handleKeyCommand(command) {
-    const {editorState} = this.state;
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return true;
-    }
-    return false;
-  }
-
-  _toggleBlockType(blockType) {
-    this.onChange(
-      RichUtils.toggleBlockType(
-        this.state.editorState,
-        blockType
-      )
-    );
-  }
-
-  _toggleInlineStyle(inlineStyle) {
-    this.onChange(
-      RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        inlineStyle
-      )
-    );
-  }
-
-  render() {
-    const {editorState} = this.state;
-
-    // If the user changes block type before entering any text, we can
-    // either style the placeholder or hide it. Let's just hide it now.
-    let className = 'EditorExample-editor';
-    var contentState = editorState.getCurrentContent();
-    if (!contentState.hasText()) {
-      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
-        className += ' EditorExample-hidePlaceholder';
-      }
-    }
-
-    return (
-      <div className="EditorExample-root">
-        <BlockStyleControls
-          editorState={editorState}
-          onToggle={this.toggleBlockType}
-        />
-        <InlineStyleControls
-          editorState={editorState}
-          onToggle={this.toggleInlineStyle}
-        />
-        <div className={className} onClick={this.focus}>
-          <Editor
-            blockStyleFn={getBlockStyle}
-            customStyleMap={styleMap}
-            editorState={editorState}
-            handleKeyCommand={this.handleKeyCommand}
-            onChange={this.onChange}
-            placeholder="Tell a story..."
-            ref="editor"
-            spellCheck={true}
-          />
-        </div>
-      </div>
-    );
-  }
-}
-
-const {Editor, EditorState, RichUtils} = Draft;
-
-// Custom overrides for "code" style.
-const styleMap = {
-  CODE: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 2,
-  },
+type Props = {};
+type State = {
+  editorState: ?EditorState;
 };
 
-function getBlockStyle(block) {
-  switch (block.getType()) {
-    case 'blockquote': return 'EditorExample-blockquote';
-    default: return null;
-  }
-}
+export default class EditorExample extends Component<Props, State> {
+  props: Props;
+  state: State;
 
-class StyleButton extends React.Component {
   constructor() {
-    super();
-    this.onToggle = (e) => {
-      e.preventDefault();
-      this.props.onToggle(this.props.style);
+    super(...arguments);
+    this.state = {editorState: undefined};
+    this._onChange = (editorState: EditorState) => {
+      this.setState({editorState});
     };
   }
 
-  render() {
-    let className = 'EditorExample-styleButton';
-    if (this.props.active) {
-      className += ' EditorExample-activeButton';
-    }
-
+  render(): React.Element {
+    let {editorState} = this.state;
     return (
-      <span className={className} onMouseDown={this.onToggle}>
-        {this.props.label}
-      </span>
+      <RichTextEditor editorState={editorState} onChange={this._onChange} />
     );
   }
 }
-
-const BLOCK_TYPES = [
-  {label: 'H1', style: 'header-one'},
-  {label: 'H2', style: 'header-two'},
-  {label: 'Blockquote', style: 'blockquote'},
-  {label: 'UL', style: 'unordered-list-item'},
-  {label: 'OL', style: 'ordered-list-item'},
-  {label: 'Code Block', style: 'code-block'},
-];
-
-const BlockStyleControls = (props) => {
-  const {editorState} = props;
-  const selection = editorState.getSelection();
-  const blockType = editorState
-    .getCurrentContent()
-    .getBlockForKey(selection.getStartKey())
-    .getType();
-
-  return (
-    <div className="EditorExample-controls">
-      {BLOCK_TYPES.map((type) =>
-        <StyleButton
-          active={type.style === blockType}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  );
-};
-
-var INLINE_STYLES = [
-  {label: 'Bold', style: 'BOLD'},
-  {label: 'Italic', style: 'ITALIC'},
-  {label: 'Underline', style: 'UNDERLINE'},
-  {label: 'Monospace', style: 'CODE'},
-];
-
-const InlineStyleControls = (props) => {
-  var currentStyle = props.editorState.getCurrentInlineStyle();
-  return (
-    <div className="EditorExample-controls">
-      {INLINE_STYLES.map((type) =>
-        <StyleButton
-          active={currentStyle.has(type.style)}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  );
-};
--- src/example.js
+++ src/example.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EditorExample from './EditorExample';
 
+let _error = console.error;
+console.error = (...args) => {
+  let message = args[0];
+  if (typeof message !== 'string' || message.indexOf('component is `contentEditable`') === -1) {
+    _error.apply(console, args);
+  }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   let rootNode = document.createElement('div');
   rootNode.classList.add('example-container');
