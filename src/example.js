03ddfe439e618ceaf006ec150c97ba876e61a786
Simon Sturmer <sstur@me.com>
2016-02-25 10:03:31 +0700

    get EditorExample working

--- src/example.js
+++ src/example.js
@@ -4,6 +4,7 @@ import EditorExample from './EditorExample';
 
 document.addEventListener('DOMContentLoaded', () => {
   let rootNode = document.createElement('div');
+  rootNode.classList.add('example-container');
   document.body.appendChild(rootNode);
   ReactDOM.render(
     <EditorExample/>,
