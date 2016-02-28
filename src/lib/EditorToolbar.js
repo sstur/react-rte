176bb7a23b4540874d33915b7d7bdbefeb757be7
Simon Sturmer <sstur@me.com>
2016-02-28 20:09:55 +0700

    refactor editor toolbar

--- src/lib/EditorToolbar.js
+++ src/lib/EditorToolbar.js
@@ -2,8 +2,10 @@
 
 import React, {Component} from 'react';
 import {EditorState, RichUtils} from 'draft-js';
-import BlockStyleControls from './BlockStyleControls';
-import InlineStyleControls from './InlineStyleControls';
+import Constants from './Constants';
+import StyleButton from './StyleButton';
+
+const {BLOCK_TYPES, INLINE_STYLES} = Constants;
 
 type ChangeHandler = (state: EditorState) => any;
 
@@ -22,19 +24,49 @@ export default class EditorToolbar extends Component<Props> {
   }
 
   render(): React.Element {
-    let {editorState} = this.props;
     return (
-      <div>
-        <BlockStyleControls
-          editorState={editorState}
-          onToggle={this._toggleBlockType}
-        />
-        <InlineStyleControls
-          editorState={editorState}
-          onToggle={this._toggleInlineStyle}
-        />
+      <div className="RichTextEditor-toolbar">
+        <div className="RichTextEditor-buttonGroup">
+          {this._renderBlockTypeButtons()}
+        </div>
+        <div className="RichTextEditor-buttonGroup">
+          {this._renderInlineStyleButtons()}
+        </div>
       </div>
     );
+  }
+
+  _renderBlockTypeButtons(): Array<React.Element> {
+    let {editorState} = this.props;
+    let selection = editorState.getSelection();
+    let blockType = editorState
+      .getCurrentContent()
+      .getBlockForKey(selection.getStartKey())
+      .getType();
+
+    return BLOCK_TYPES.map((type, index) => (
+      <StyleButton
+        key={String(index)}
+        active={type.style === blockType}
+        label={type.label}
+        onToggle={this._toggleBlockType}
+        style={type.style}
+      />
+    ));
+  }
+
+  _renderInlineStyleButtons(): Array<React.Element> {
+    let {editorState} = this.props;
+    let currentStyle = editorState.getCurrentInlineStyle();
+    return INLINE_STYLES.map((type, index) => (
+      <StyleButton
+        key={String(index)}
+        active={currentStyle.has(type.style)}
+        label={type.label}
+        onToggle={this._toggleInlineStyle}
+        style={type.style}
+      />
+    ));
   }
 
   _toggleBlockType(blockType: string) {
