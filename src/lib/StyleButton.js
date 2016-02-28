169327753b9fa6a9bc554ed708a2b5163976e8e0
Simon Sturmer <sstur@me.com>
2016-02-28 07:44:36 +0700

    StyleButton: use ui-icon-button

--- src/lib/StyleButton.js
+++ src/lib/StyleButton.js
@@ -1,13 +1,14 @@
 /* @flow */
-import React from 'react';
-
-const {Component} = React;
+import React, {Component} from 'react';
+import IconButton from '../ui/IconButton';
+import cx from 'classnames';
 
 type Props = {
   active: boolean;
   style: string;
   label: string;
-  onToggle: (style: string) => any
+  onToggle: (style: string) => any;
+  className: ?string;
 };
 
 export default class StyleButton extends Component<Props> {
@@ -15,22 +16,30 @@ export default class StyleButton extends Component<Props> {
 
   constructor() {
     super(...arguments);
-    this.onToggle = (event) => {
-      event.preventDefault();
-      this.props.onToggle(this.props.style);
-    };
+    this._onToggle = this._onToggle.bind(this);
   }
 
   render(): React.Element {
-    let className = 'RichTextEditor-styleButton';
-    if (this.props.active) {
-      className += ' RichTextEditor-activeButton';
-    }
-
+    let {props} = this;
+    let className = cx(props.className, {
+      'RichTextEditor-styleButton': true,
+      'RichTextEditor-activeButton': props.active,
+    });
+    let iconName = props.style.toLowerCase();
+    // `focusOnClick` will prevent the editor from losing focus when a control
+    // button is clicked.
     return (
-      <span className={className} onMouseDown={this.onToggle}>
-        {this.props.label}
-      </span>
+      <IconButton
+        className={className}
+        label={props.label}
+        iconName={iconName}
+        onClick={this._onToggle}
+        focusOnClick={false}
+      />
     );
+  }
+
+  _onToggle() {
+    this.props.onToggle(this.props.style);
   }
 }
