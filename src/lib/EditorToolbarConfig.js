2e3935f5ec0c1eeb516ca3f577df38426b7b759e
Simon Sturmer <sstur@me.com>
2016-03-13 13:02:10 +0800

    add EditorToolbarConfig

---
+++ src/lib/EditorToolbarConfig.js
@@ -0,0 +1,25 @@
+export const INLINE_STYLE_BUTTONS = [
+  {label: 'Bold', style: 'BOLD'},
+  {label: 'Italic', style: 'ITALIC'},
+  {label: 'Strikethrough', style: 'STRIKETHROUGH'},
+  {label: 'Monospace', style: 'CODE'},
+];
+
+export const BLOCK_TYPE_DROPDOWN = [
+  {label: 'Normal', style: 'unstyled'},
+  {label: 'Heading Large', style: 'header-one'},
+  {label: 'Heading Medium', style: 'header-two'},
+  {label: 'Heading Small', style: 'header-three'},
+  {label: 'Code Block', style: 'code-block'},
+];
+export const BLOCK_TYPE_BUTTONS = [
+  {label: 'UL', style: 'unordered-list-item'},
+  {label: 'OL', style: 'ordered-list-item'},
+  {label: 'Blockquote', style: 'blockquote'},
+];
+
+export default {
+  INLINE_STYLE_BUTTONS,
+  BLOCK_TYPE_DROPDOWN,
+  BLOCK_TYPE_BUTTONS,
+};
