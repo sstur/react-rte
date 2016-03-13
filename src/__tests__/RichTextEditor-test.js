7393817bfa9b833f20567f4d6a318fcd064826a4
Simon Sturmer <sstur@me.com>
2016-03-13 08:22:13 +0800

    add react-addons-test-utils for tests

---
+++ src/__tests__/RichTextEditor-test.js
@@ -0,0 +1,17 @@
+/* @flow */
+const {describe, it} = global;
+import React from 'react';
+import {createRenderer} from 'react-addons-test-utils';
+import expect from 'expect';
+import RichTextEditor, {createEmptyValue} from '../RichTextEditor';
+
+describe('RichTextEditor', () => {
+  it('should render', () => {
+    let renderer = createRenderer();
+    let value = createEmptyValue();
+    renderer.render(<RichTextEditor value={value} />);
+    let output = renderer.getRenderOutput();
+    expect(output.type).toEqual('div');
+    expect(output.props.className).toEqual('rte-root');
+  });
+});
