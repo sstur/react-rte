294afea1d44ec18a75fe4b186a68e4e126eb8532
Simon Sturmer <sstur@me.com>
2016-04-09 18:00:30 +0700

    remove console.error hack

--- src/demo.js
+++ src/demo.js
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import EditorDemo from './EditorDemo';
 
-let _error = console.error;
-console.error = (...args) => {
-  let message = args[0];
-  if (typeof message !== 'string' || message.indexOf('component is `contentEditable`') === -1) {
-    _error.apply(console, args);
-  }
-};
-
 document.addEventListener('DOMContentLoaded', () => {
   let rootNode = document.createElement('div');
   document.body.appendChild(rootNode);
