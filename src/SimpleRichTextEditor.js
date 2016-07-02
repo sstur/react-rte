06ed718a463e6b8ddc0fb88fd868727c475a0daf
Simon Sturmer <sstur@me.com>
2016-07-02 20:40:07 +0700

    Add experimental SimpleRichTextEditor

---
+++ src/SimpleRichTextEditor.js
@@ -0,0 +1,63 @@
+/* @flow */
+import React, {Component} from 'react';
+import RichTextEditor, {createEmptyValue} from './RichTextEditor';
+import autobind from 'class-autobind';
+
+import type {EditorValue} from './RichTextEditor';
+
+type Props = {
+  value: string;
+  format: string;
+  onChange: (value: string) => any;
+};
+type State = {
+  editorValue: EditorValue;
+};
+
+export default class SimpleRichTextEditor extends Component {
+  props: Props;
+  state: State;
+
+  constructor() {
+    super(...arguments);
+    autobind(this);
+    this.state = {
+      editorValue: createEmptyValue(),
+    };
+  }
+
+  componentWillMount() {
+    this._updateStateFromProps(this.props);
+  }
+
+  componentWillReceiveProps(newProps: Props) {
+    this._updateStateFromProps(newProps);
+  }
+
+  _updateStateFromProps(props: Props) {
+    let {value, format} = props;
+    let {editorValue} = this.state;
+    this.setState({
+      editorValue: editorValue.setContentFromString(value, format),
+    });
+  }
+
+  render(): React.Element {
+    let {value, format, onChange, ...otherProps} = this.props; // eslint-disable-line no-unused-vars
+    return (
+      <RichTextEditor
+        {...otherProps}
+        value={this.state.editorValue}
+        onChange={this._onChange}
+      />
+    );
+  }
+
+  _onChange(editorValue: EditorValue) {
+    let {format, onChange} = this.props;
+    if (onChange != null) {
+      let stringValue = editorValue.toString(format);
+      onChange(stringValue);
+    }
+  }
+}
