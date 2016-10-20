be2e5faa4af6628be4910ad1725fc7be101e946f
Hyunyoung Cho <zerohch0@gmail.com>
2016-10-20 11:13:32 +0900

    Add webpack.DefinePlugin (#109)
    * Add webpack.DefinePlugin
    
    * for CI and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,11 @@ module.exports = [{
   },
   module: {loaders: loaders},
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
     new webpack.optimize.UglifyJsPlugin({
       beautify: true,
       comments: true,
