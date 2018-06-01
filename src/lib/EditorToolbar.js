a48bb2a95e6d5e069ed55343b26706f793979842
Tim van der Horst <timgvdh@gmail.com>
2018-06-01 18:16:02 +1000

    Added custom label support for link, image, & history buttons

--- src/lib/EditorToolbar.js
+++ src/lib/EditorToolbar.js
@@ -197,11 +197,16 @@ export default class EditorToolbar extends Component {
     let isCursorOnLink = (entity != null && entity.type === ENTITY_TYPE.LINK);
     let shouldShowLinkButton = hasSelection || isCursorOnLink;
     let defaultValue = (entity && isCursorOnLink) ? entity.getData().url : '';
+    const config = toolbarConfig.LINK_BUTTONS || {};
+    const linkConfig = config.link || {};
+    const removeLinkConfig = config.removeLink || {};
+    const linkLabel = linkConfig.label || 'Link';
+    const removeLinkLabel = removeLinkConfig.label || 'Remove Link';
 
     return (
       <ButtonGroup key={name}>
         <PopoverIconButton
-          label="Link"
+          label={linkLabel}
           iconName="link"
           isDisabled={!shouldShowLinkButton}
           showPopover={this.state.showLinkInput}
@@ -211,7 +216,7 @@ export default class EditorToolbar extends Component {
         />
         <IconButton
           {...toolbarConfig.extraProps}
-          label="Remove Link"
+          label={removeLinkLabel}
           iconName="remove-link"
           isDisabled={!isCursorOnLink}
           onClick={this._removeLink}
@@ -221,11 +226,13 @@ export default class EditorToolbar extends Component {
     );
   }
 
-  _renderImageButton(name: string) {
+  _renderImageButton(name: string, toolbarConfig: ToolbarConfig) {
+    const config = (toolbarConfig.IMAGE_BUTTON || {});
+    const label = config.label || 'Image';
     return (
       <ButtonGroup key={name}>
         <PopoverIconButton
-          label="Image"
+          label={label}
           iconName="image"
           showPopover={this.state.showImageInput}
           onTogglePopover={this._toggleShowImageInput}
@@ -239,11 +246,16 @@ export default class EditorToolbar extends Component {
     let {editorState} = this.props;
     let canUndo = editorState.getUndoStack().size !== 0;
     let canRedo = editorState.getRedoStack().size !== 0;
+    const config = toolbarConfig.HISTORY_BUTTONS || {};
+    const undoConfig = config.undo || {};
+    const redoConfig = config.redo || {};
+    const undoLabel = undoConfig.label || 'Undo';
+    const redoLabel = redoConfig.label || 'Redo';
     return (
       <ButtonGroup key={name}>
         <IconButton
           {...toolbarConfig.extraProps}
-          label="Undo"
+          label={undoLabel}
           iconName="undo"
           isDisabled={!canUndo}
           onClick={this._undo}
@@ -251,7 +263,7 @@ export default class EditorToolbar extends Component {
         />
         <IconButton
           {...toolbarConfig.extraProps}
-          label="Redo"
+          label={redoLabel}
           iconName="redo"
           isDisabled={!canRedo}
           onClick={this._redo}
