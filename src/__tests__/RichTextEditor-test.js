61addc58ee7bdc37718222f85409bf4f46adf027
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2022-07-18 16:02:36 +0300

    feat: add accessibility properties for ariaLabel, ariaDescribedBy and ariaLabelledBy

--- src/__tests__/RichTextEditor-test.js
+++ src/__tests__/RichTextEditor-test.js
@@ -1,18 +1,35 @@
 /* @flow */
 const {describe, it} = global;
 import React from 'react';
-import ShallowRenderer from 'react-test-renderer/shallow';
+// import ShallowRenderer from 'react-test-renderer/shallow';
 import expect from 'expect';
+import {render, screen} from '@testing-library/react';
 import RichTextEditor, {createEmptyValue} from '../RichTextEditor';
 
 describe('RichTextEditor', () => {
   it('should render', () => {
-    let renderer = new ShallowRenderer();
     let value = createEmptyValue();
-    renderer.render(<RichTextEditor value={value} />);
-    let output = renderer.getRenderOutput();
-    expect(output.type).toEqual('div');
-    expect(output.props.className).toBeA('string');
-    expect(output.props.className).toInclude('RichTextEditor__root');
+    const {container} = render(<RichTextEditor value={value} ariaLabel="Hello Westeros" />);
+    expect(container.querySelector('div').className).toInclude('RichTextEditor__root___');
+  });
+
+  describe('accessibility', () => {
+    it('should have the given ariaLabel', () => {
+      let value = createEmptyValue();
+      render(<RichTextEditor value={value} ariaLabel="Hello Westeros" />);
+      expect(screen.getByRole('textbox').getAttribute('aria-label')).toEqual('Hello Westeros');
+    });
+
+    it('should have the given ariaLabelledBy', () => {
+      let value = createEmptyValue();
+      render(<RichTextEditor value={value} ariaLabelledBy="jonsnow" />);
+      expect(screen.getByRole('textbox').getAttribute('aria-labelledby')).toEqual('jonsnow');
+    });
+
+    it('should have the given ariaDescribedBy', () => {
+      let value = createEmptyValue();
+      render(<RichTextEditor value={value} ariaDescribedBy="sunandmoon" />);
+      expect(screen.getByRole('textbox').getAttribute('aria-describedby')).toEqual('sunandmoon');
+    });
   });
 });
